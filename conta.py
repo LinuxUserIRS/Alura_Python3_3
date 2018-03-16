f7174c29fc16fb6d2ae77dab7725785cd22e9ec9
Italo Soares <irs@cin.ufpe.br>
2018-03-16 17:32:08 -0300

    Criação da classe conta

---
+++ conta.py
@@ -0,0 +1,7 @@
+class Conta:
+    def __init__(self, numero, titular, saldo, limite):
+        print("Construindo objeto...{}".format(self))
+        self.numero = 123
+        self.titular = "Nico"
+        self.saldo = 55.0
+        self.limite = 1000.0
