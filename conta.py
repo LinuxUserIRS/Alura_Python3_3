e9aabeb06d55f32595f11fb4baf4586c78208503
Italo Soares <irs@cin.ufpe.br>
2018-03-28 16:46:44 -0300

    Refatoração e implementação da função de transferência.

--- conta.py
+++ conta.py
@@ -1,19 +1,26 @@
 class Conta:
+    #Função __init__ é o construtor da classe
+    #Parâmetro limite é padrão
     def __init__(self, num, name, balance, limit=1000):
         print("Construindo objeto...{}".format(self))
-        self.num = num
-        self.name = name
-        self.balance = balance
-        self.limit = limit
+        #Os __ na frente dos atributos os deixam como privados
+        self.__num = num
+        self.__name = name
+        self.__balance = balance
+        self.__limit = limit
 
     def extrato(self):
-        return self.balance
+        return self.__balance
 
     def deposita(self, num):
-        self.balance += num
+        self.__balance += num
 
     def saca(self, num):
-        if self.balance-num > 0:
-            self.balance -= num
+        if self.__balance-num > 0:
+            self.__balance -= num
         else:
             print("Você não tem saldo suficiente")
+
+    def transfere(self, Conta, num):
+        self.saca(num)
+        Conta.deposita(num)
